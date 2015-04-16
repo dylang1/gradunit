9a27898fd4e3d736bfd0e9d5199da9754f1e7035
Dylan <dylangilmour12345@hotmail.co.uk>
2015-04-17 00:28:37 +0100

    Initial upload and creation
    Initail upload and creation of project

---
+++ GradedUnit/GradedUnit/Game.cs
@@ -0,0 +1,106 @@
+﻿
+#region Using Statements
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+#endregion
+
+namespace GradedUnit
+{
+    /// <summary>
+    /// This main game class is extremely simple: all the interesting
+    /// stuff happens in the ScreenManager component.
+    /// </summary>
+    public class GradedUnitGame : Microsoft.Xna.Framework.Game
+    {
+        #region Fields
+
+        GraphicsDeviceManager graphics;
+        ScreenManager screenManager;
+
+
+        // By preloading any assets used by UI rendering, we avoid framerate glitches
+        // when they suddenly need to be loaded in the middle of a menu transition.
+        static readonly string[] preloadAssets =
+        {
+            "gradient",
+        };
+
+
+        #endregion
+
+        #region Initialization
+
+
+        /// <summary>
+        /// The main game constructor.
+        /// </summary>
+        public GradedUnitGame()
+        {
+            Content.RootDirectory = "Content";
+
+            graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 853;
+            graphics.PreferredBackBufferHeight = 480;
+
+            // Create the screen manager component.
+            screenManager = new ScreenManager(this);
+
+            Components.Add(screenManager);
+
+            // Activate the first screens.
+            screenManager.AddScreen(new BackgroundScreen(), null);
+            screenManager.AddScreen(new MainMenuScreen(), null);
+        }
+
+
+        /// <summary>
+        /// Loads graphics content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            foreach (string asset in preloadAssets)
+            {
+                Content.Load<object>(asset);
+            }
+        }
+
+
+        #endregion
+
+        #region Draw
+
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        protected override void Draw(GameTime gameTime)
+        {
+            graphics.GraphicsDevice.Clear(Color.Black);
+
+            // The real drawing happens inside the screen manager component.
+            base.Draw(gameTime);
+        }
+
+
+        #endregion
+    }
+
+
+    #region Entry Point
+
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    static class Program
+    {
+        static void Main()
+        {
+            using (GradedUnitGame game = new GradedUnitGame())
+            {
+                game.Run();
+            }
+        }
+    }
+
+    #endregion
+}
